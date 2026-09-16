bcb3469dd442256e71ca0e67625669a3639a6ad2
Chathu8616 <chathurangabandara8616@gmail.com>
2026-09-16 13:13:47 +0530

    Create Spring Boot backend

---
+++ echanneling-backend/src/main/java/lk/ac/sliit/echanneling_backend/EchannelingBackendApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.echanneling_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchannelingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchannelingBackendApplication.class, args);
+	}
+
+}
---
+++ echanneling-backend/src/test/java/lk/ac/sliit/echanneling_backend/EchannelingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.sliit.echanneling_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchannelingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
